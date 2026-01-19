f378a0480e60be2ff9a5f0f328cc6b79ae2ad87c
matejaradeta <mateja.radeta@gmail.com>
2026-01-19 18:35:48 +0100

    Initialize map interaction with layer selection

---
+++ script.js
@@ -0,0 +1,24 @@
+const map = document.getElementById("map");
+const ctx = map.getContext("2d");
+
+let selectedLayer = null;
+
+document.querySelectorAll("#layers button").forEach(btn => {
+  btn.onclick = () => {
+    selectedLayer = btn.dataset.layer;
+    console.log("Layer:", selectedLayer);
+  };
+});
+
+map.addEventListener("click", e => {
+  if (!selectedLayer) {
+    alert("Choose a layer first!");
+    return;
+  }
+
+  const rect = map.getBoundingClientRect();
+  const mx = e.clientX - rect.left;
+  const my = e.clientY - rect.top;
+
+  console.log("Map click:", mx, my);
+});
